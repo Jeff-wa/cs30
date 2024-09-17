d8b98b6da9dc52b366592d68aec30aaa5a3cd91e
Jeff-wa <wxy45549830@163.com>
2024-09-17 12:16:18 -0600

    Create sketch.js

---
+++ 02 user interactions/sketch.js
@@ -0,0 +1,29 @@
+// Project Title
+// Your Name
+// Date
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(0,50,120);
+
+  fill(110);
+  triangle(0,700,1000,700,500,300);
+
+  fill(160);
+  triangle(150,700,500,700,350,130);
+
+  fill(140)
+  triangle(0,700,300,700,200,300)
+
+  fill(90,150,0)
+  rect(0,700,2000,700,0,0,0,0)
+
+  fill(180,180,0)
+  circle(0,0,300)
+}
